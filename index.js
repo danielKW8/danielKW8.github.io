f79561f8def2bc16a38b4fba6aa005c8137e1208
danielKW8 <dankurnz@gmail.com>
2023-12-19 09:26:56 +0700

    login page

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
-setInterval(showNumber, 1);
- 
+
 var number = 0;
+var welcomeMsg ="";
+
 
 function add(){
     number++;
+    document.getElementById("number").innerHTML = number;
 }
 
 function subtract(){
     number--;
-}
-
-function showNumber(){
     document.getElementById("number").innerHTML = number;
 }
+
+function submitForm() {
+    // Get user input from form element(s)
+    const userName = document.getElementById("username").value; // Assuming an "username" input field
+  
+    // Optional: additional processing or validation
+  
+    // Redirect to next page with user input parameters
+    window.location.href = `dashboardPage.html?userName=${userName}`; // Replace "userName" with your actual parameter name based on form submission method (e.g., `name` attribute)
+  
+    return false; // Prevent default form submission (optional)
+  }
+  
+  
